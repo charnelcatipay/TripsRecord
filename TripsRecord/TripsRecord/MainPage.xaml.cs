1883c6080cd00ded955a659008a35118580e249f
Charnel Catipay <ccatipay@gmail.com>
2018-03-23 17:54:06 +0800

    Add project files.

---
+++ TripsRecord/TripsRecord/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TripsRecord
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void loginButton_Clicked(object sender, EventArgs e)
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
+
+            if (isEmailEmpty || isPasswordEmpty)
+            { }
+            else
+            {
+               Navigation.PushAsync(new HomePage());
+            }
+        }
+
+    }
+}
