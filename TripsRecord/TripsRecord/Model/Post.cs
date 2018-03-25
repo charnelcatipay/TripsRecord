587ba9d8025220e8a8ec91b5e4cda30ef6db294e
charnelcatipay <charnel.catipay@gmail.com>
2018-03-25 16:15:09 +0800

    Implementing the INotifyChanged

--- TripsRecord/TripsRecord/Model/Post.cs
+++ TripsRecord/TripsRecord/Model/Post.cs
@@ -1,26 +1,131 @@
 ﻿using SQLite;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TripsRecord.Model
 {
-    public class Post
+    public class Post: INotifyPropertyChanged
     {
-        [PrimaryKey, AutoIncrement]
-        public string Id { get; set; }
-        [MaxLength(250)]
-        public string  Experience { get; set; }
-        public string VenueName { get; set; }
-        public string CategoryId { get; set; }
-        public string CategoryName { get; set; }
-        public string Address { get; set; }
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
-        public int Distance { get; set; }
-        public string UserId { get; set; }
+        private string id;
+
+        public string Id
+        {
+            get { return id; }
+            set {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+
+        private string experience;
+
+        public string Experience
+        {
+            get { return experience; }
+            set {
+                experience = value;
+                OnPropertyChanged("Experience");
+            }
+        }
+
+        private string venueName;
+
+        public string VenueName
+        {
+            get { return venueName; }
+            set {
+                venueName = value;
+                OnPropertyChanged("VenueName");
+            }
+        }
+
+        private string categoryId;
+
+        public string CategoryId
+        {
+            get { return categoryId; }
+            set {
+                categoryId = value;
+                OnPropertyChanged("CategoryId");
+            }
+        }
+
+        private string categoryName;
+
+        public string CategoryName
+        {
+            get { return categoryName; }
+            set {
+                categoryName = value;
+                OnPropertyChanged("CategoryName");
+            }
+        }
+
+        private string address;
+
+        public string Address
+        {
+            get { return address; }
+            set {
+                address = value;
+                OnPropertyChanged("Address");
+            }
+        }
+
+        private double latitude;
+
+        public double Latitude
+        {
+            get { return latitude; }
+            set {
+                latitude = value;
+                OnPropertyChanged("Latitude");
+            }
+        }
+
+        private double longitude;
+
+        public double Longitude
+        {
+            get { return longitude; }
+            set {
+                longitude = value;
+                OnPropertyChanged("Longitude");
+            }
+        }
+
+        private int distance;
+
+        public int Distance
+        {
+            get { return distance; }
+            set {
+                distance = value;
+                OnPropertyChanged("Distance");
+            }
+        }
+
+        private string userId;
+
+        public string UserId
+        {
+            get { return userId; }
+            set {
+                userId = value;
+                OnPropertyChanged("UserId");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
 
         public static async void Insert(Post post)
         {
