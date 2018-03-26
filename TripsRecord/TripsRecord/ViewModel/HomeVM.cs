3895ff01c9d05283702664d97b2c79e73ef46c7d
charnelcatipay <charnel.catipay@gmail.com>
2018-03-26 13:15:40 +0800

    Implementing IComamnd

---
+++ TripsRecord/TripsRecord/ViewModel/HomeVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TripsRecord.ViewModel.Commands;
+
+namespace TripsRecord.ViewModel
+{
+    public class HomeVM
+    {
+        public NavigationCommand NavCommand;
+
+        public HomeVM()
+        {
+            NavCommand = new NavigationCommand(this);
+        }
+
+        public void Navigate()
+        {
+            //Todo
+        }
+    }
+}
