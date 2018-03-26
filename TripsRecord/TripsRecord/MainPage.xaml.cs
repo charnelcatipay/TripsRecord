9a7414560a8bc8d0f06c477978526a8725269293
charnelcatipay <charnel.catipay@gmail.com>
2018-03-26 14:45:58 +0800

    Command Parameters

--- TripsRecord/TripsRecord/MainPage.xaml.cs
+++ TripsRecord/TripsRecord/MainPage.xaml.cs
@@ -4,18 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TripsRecord.Model;
+using TripsRecord.ViewModel.Commands;
 using Xamarin.Forms;
 
 namespace TripsRecord
 {
 	public partial class MainPage : ContentPage
 	{
+        MainVM viewModel;
 		public MainPage()
 		{
 			InitializeComponent();
 
             var assembly = typeof(MainPage);
 
+            viewModel = new MainVM();
+            BindingContext = viewModel;
 
             iconImage.Source = ImageSource.FromResource("TripsRecord.Assets.Images.manphone.png", assembly);
 		}
---
+++ TripsRecord/TripsRecord/ViewModel/MainVM.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using TripsRecord.Model;
+
+namespace TripsRecord.ViewModel.Commands
+{
+    public class MainVM : INotifyPropertyChanged
+    {
+        private User user;
+
+        public User User
+        {
+            get { return user; }
+            set {
+                user = value;
+                OnPropertyChanged("User");
+            }
+        }
+
+        public LoginCommand LoginCommand { get; set; }
+
+        private string email;
+
+        public string Email
+        {
+            get { return email; }
+            set {
+                email = value;
+                User = new User()
+                {
+                    Email = this.Email,
+                    Password = this.Password
+                };
+                OnPropertyChanged("Email");
+            }
+        }
+
+        private string password;
+
+        public string Password
+        {
+            get { return password; }
+            set {
+                password = value;
+                User = new User()
+                {
+                    Email = this.Email,
+                    Password = this.Password
+                };
+                OnPropertyChanged("Password");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public MainVM()
+        {
+            User = new User();
+            LoginCommand = new LoginCommand(this);
+        }
+
+        public async void Login()
+        {
+            bool canLogin = await User.Login(User.Email, User.Password);
+            if (canLogin)
+                await App.Current.MainPage.Navigation.PushAsync(new HomePage());
+            else
+                await App.Current.MainPage.DisplayAlert("Error", "Email or password are incorrect", "Ok");
+        }
+    }
+}
